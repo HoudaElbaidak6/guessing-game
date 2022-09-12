56bab79b8ff64ff9d6893e0950edd0ba2775f914
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-12 09:14:20 +0100

    DONE ALMOST

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ let button = document.querySelector("button");
 let essays = document.getElementById("chance");
 let userGuess = document.getElementById("input");
 let result = document.getElementById("results");
-let audios=document.querySelectorAll("audio")
-let numMax, numMin, chancenbr, nombreAleatoire;
+let audios = document.querySelectorAll("audio");
+let chancenbr, nombreAleatoire;
 
 function selection(min, max, chance) {
   instructions.textContent = `Enter a number btw ${min} and ${max}`;
   essays.textContent = `You have ${chance} chances to guess the right number`;
   let randomNumber = Math.floor(Math.random() * max) + min;
-  numMin = min;
-  numMax = max;
   chancenbr = chance;
   nombreAleatoire = randomNumber;
   result.innerText = "";
@@ -21,12 +19,15 @@ function selection(min, max, chance) {
 
 function easylvl() {
   selection(1, 10, 3);
+  console.log(nombreAleatoire);
 }
 function mlvl() {
   selection(1, 100, 5);
+  console.log(nombreAleatoire);
 }
 function hardlvl() {
   selection(1, 1000, 10);
+  console.log(nombreAleatoire);
 }
 
 function gameOver() {
@@ -40,7 +41,6 @@ function Restart() {
 }
 
 function guessing() {
-  console.log(nombreAleatoire);
   let userGuessValue = Number(userGuess.value);
 
   if (userGuessValue > nombreAleatoire) {
@@ -48,7 +48,7 @@ function guessing() {
     chancenbr--;
   } else if (userGuessValue === nombreAleatoire) {
     result.innerText = "Good job !! ";
-    result.style.color="green"
+    result.style.color = "green";
     nombreTrouve = true;
   } else {
     result.innerText = `${userGuessValue} is low , enter a higher number`;
@@ -57,13 +57,13 @@ function guessing() {
 
   if (chancenbr === 0 && nombreTrouve === false) {
     result.innerText = "You lost ";
-    result.style.color="red"
+    result.style.color = "red";
     audios[1].play();
     gameOver();
   }
   if (nombreTrouve === true) {
     result.innerText = "You won";
-    result.style.color="green"
+    result.style.color = "green";
     audios[0].play();
     gameOver();
   }
